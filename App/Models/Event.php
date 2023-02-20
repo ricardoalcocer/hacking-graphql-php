e9617512dcb6a714d30748d6dd57939b8a7aa45a
alco <alco@ricardoalcocer.com>
2023-02-20 11:55:03 -0600

    remove unused relationships

--- App/Models/Event.php
+++ App/Models/Event.php
@@ -8,19 +8,4 @@
         protected $table    = 'events';
         public $timestamps  = false;
 
-        public function artist(){
-            return $this->hasOne(Artist::class,'id','artist_id');
-        }
-
-        public function host(){
-            return $this->hasOne(Host::class,'id','host_id');
-        }
-
-        public function venue(){
-            return $this->hasOne(Venue::class,'id','venue_id');
-        }
-
-        public function format(){
-            return $this->hasOne(EventFormat::class,'id','format_id');
-        }
     }    
